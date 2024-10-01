1961fa58a73f2c9a7879cf028d1cda621fed3936
Alex-Sm-KH <oleksandr1020s@gmail.com>
2024-10-01 15:00:50 +0300

    0 50 0 50 Clean (LEDS_WAVE multi problem)

--- Lab_4_Smiian/Core/Src/main.c
+++ Lab_4_Smiian/Core/Src/main.c
@@ -18,38 +18,44 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "tim.h"
-#include "gpio.h"
+
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "tim.h"
+#include "gpio.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 typedef enum
 {
-    LEDS_PWM_CH_RED = TIM_CHANNEL_1,
-    LEDS_PWM_CH_BLUE = TIM_CHANNEL_2,
-    LEDS_PWM_CH_GREEN = TIM_CHANNEL_3,
-    LEDS_PWM_CH_ORANGE = TIM_CHANNEL_4 
-}LEDS_PWM_TIM_CH_e_t;
+    LEDS_PWM_CALLB = 0
+}flags_e_t;
 
 typedef enum
 {
-    LEDS = 0,
-    DIR_RED,
-    DIR_BLUE,
-    DIR_GREEN,
-    DIR_ORANGE
-}FLAGS_e_t;
+    RED = 0,
+    BLUE,
+    GREEN,
+    ORANGE,
+    LEDS_ALL
+}leds_color_e_t;
 
 typedef enum
 {
     REVERSE = 0,
     FORWARD
-}DIRECTIONS_e_t;
+}directions_e_t;
+
+typedef struct
+{
+    directions_e_t led_dir;
+    uint8_t        led_ptr;
+    uint32_t       led_ch;
+}leds_config_s_t;
+
+leds_config_s_t leds_config[LEDS_ALL] = {0};
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -59,125 +65,89 @@ typedef enum
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-#define __FLAG_SET(flag_bit) main_flags|=(1<<flag_bit)
-#define __FLAG_RESET(flag_bit) main_flags&=(~(1<<flag_bit))
-#define __FLAG_CHECK(flag_bit) main_flags&=(1<<flag_bit)
+#define __FLAG_SET(flag_bit)   main_flags |= (1 << flag_bit)
+#define __FLAG_RESET(flag_bit) main_flags &= (~ (1 << flag_bit))
+#define __FLAG_CHECK(flag_bit) main_flags &= (1 << flag_bit)
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
 uint32_t main_flags = 0;
-uint8_t RED_pointer = 0;
-uint8_t BLUE_pointer = 0;
-uint8_t GREEN_pointer = 0;
-uint8_t ORANGE_pointer = 0;
-uint16_t LEDS_WAVE_LENGTH = 255;
-uint8_t LEDS_wave[256] = 
-{
-    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    1,    1,    1,    1,    1,    1,
-    1,    1,    1,    2,    2,    2,    2,    2,    2,    2,    2,    3,    3,    3,    3,    3,
-    3,    3,    4,    4,    4,    4,    4,    5,    5,    5,    5,    5,    6,    6,    6,    6,
-    6,    7,    7,    7,    7,    8,    8,    8,    8,    9,    9,    9,   10,   10,   10,   11,
-   11,   11,   12,   12,   12,   13,   13,   13,   14,   14,   14,   15,   15,   16,   16,   16,
-   17,   17,   18,   18,   19,   19,   20,   20,   21,   21,   22,   22,   23,   23,   24,   24,
-   25,   25,   26,   26,   27,   28,   28,   29,   29,   30,   31,   31,   32,   33,   33,   34,
-   35,   35,   36,   37,   37,   38,   39,   40,   40,   41,   42,   43,   44,   44,   45,   46,
-   47,   48,   49,   49,   50,   51,   52,   53,   54,   55,   56,   57,   58,   59,   60,   61,
-   62,   63,   64,   65,   66,   67,   68,   69,   70,   71,   72,   73,   75,   76,   77,   78,
-   79,   80,   82,   83,   84,   85,   87,   88,   89,   90,   92,   93,   94,   96,   97,   99,
-  100,  101,  103,  104,  106,  107,  108,  110,  111,  113,  114,  116,  118,  119,  121,  122,
-  124,  125,  127,  129,  130,  132,  134,  135,  137,  139,  141,  142,  144,  146,  148,  149,
-  151,  153,  155,  157,  159,  161,  162,  164,  166,  168,  170,  172,  174,  176,  178,  180,
-  182,  185,  187,  189,  191,  193,  195,  197,  200,  202,  204,  206,  208,  211,  213,  215,
-  218,  220,  222,  225,  227,  230,  232,  234,  237,  239,  242,  244,  247,  249,  252,  255,
-};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-void LEDS_PWM_Start (uint16_t RED_Start_Point, uint16_t BLUE_Start_Point, uint16_t GREEN_Start_Point, uint16_t ORANGE_Start_Point);
+void LEDS_PWM_Start (uint8_t RED_Start_Point, uint8_t BLUE_Start_Point, uint8_t GREEN_Start_Point, uint8_t ORANGE_Start_Point);
 void LEDS_PWM_Step ();
-void LEDS_PWM_Ind_Step (uint16_t LED_pointer, LEDS_PWM_TIM_CH_e_t TIM_CH, FLAGS_e_t DIR_LED);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void Leds_PWM_Start (uint16_t RED_Start_Point, uint16_t BLUE_Start_Point, uint16_t GREEN_Start_Point, uint16_t ORANGE_Start_Point)
+void Leds_PWM_Start (uint8_t RED_Start_Point, uint8_t BLUE_Start_Point, uint8_t GREEN_Start_Point, uint8_t ORANGE_Start_Point)
 {
-    if ((RED_Start_Point > LEDS_WAVE_LENGTH) || (RED_Start_Point < 0))
+    uint8_t start_points [LEDS_ALL] = {RED_Start_Point, BLUE_Start_Point, GREEN_Start_Point, ORANGE_Start_Point};  
+    for(leds_color_e_t color = RED; color < LEDS_ALL; color++)
     {
-        RED_Start_Point = 0;
-    }
-    if ((BLUE_Start_Point > LEDS_WAVE_LENGTH) || (BLUE_Start_Point < 0))
-    {
-        BLUE_Start_Point = 0;
+        leds_config[color].led_dir = FORWARD;
+        if (start_points [color] > MAX_VALUE)
+        {
+            leds_config[color].led_ptr = MAX_VALUE;
+        }
+        else
+        {
+            leds_config[color].led_ptr = start_points[color];
+        }
     } 
-    if ((GREEN_Start_Point > LEDS_WAVE_LENGTH) || (GREEN_Start_Point < 0))
-    {
-        GREEN_Start_Point = 0;
-    } 
-    if ((ORANGE_Start_Point > LEDS_WAVE_LENGTH) || (ORANGE_Start_Point < 0))
-    {
-        ORANGE_Start_Point = 0;
-    }     
-    RED_pointer = RED_Start_Point;
-    BLUE_pointer = BLUE_Start_Point;
-    GREEN_pointer = GREEN_Start_Point;
-    ORANGE_pointer = ORANGE_Start_Point;
-    __FLAG_SET(DIR_RED);
-    __FLAG_SET(DIR_BLUE);
-    __FLAG_SET(DIR_GREEN);
-    __FLAG_SET(DIR_ORANGE);
-    HAL_TIM_PWM_Start_IT (&LEDS_PWM_TIM, LEDS_PWM_CH_RED);
-    HAL_TIM_PWM_Start_IT (&LEDS_PWM_TIM, LEDS_PWM_CH_BLUE);
-    HAL_TIM_PWM_Start_IT (&LEDS_PWM_TIM, LEDS_PWM_CH_GREEN);
-    HAL_TIM_PWM_Start_IT (&LEDS_PWM_TIM, LEDS_PWM_CH_ORANGE);
+    leds_config[RED].led_ch    = TIM_CHANNEL_1;
+    leds_config[BLUE].led_ch   = TIM_CHANNEL_2;
+    leds_config[GREEN].led_ch  = TIM_CHANNEL_3;
+    leds_config[ORANGE].led_ch = TIM_CHANNEL_4;
+    __HAL_TIM_ENABLE_IT (&LEDS_PWM_TIM, TIM_IT_UPDATE);
+    HAL_TIM_PWM_Start (&LEDS_PWM_TIM, leds_config[RED].led_ch);
+    HAL_TIM_PWM_Start (&LEDS_PWM_TIM, leds_config[BLUE].led_ch);
+    HAL_TIM_PWM_Start (&LEDS_PWM_TIM, leds_config[GREEN].led_ch);
+    HAL_TIM_PWM_Start (&LEDS_PWM_TIM, leds_config[ORANGE].led_ch);
 }
 
 void Leds_PWM_Step ()
 {
-    LEDS_PWM_Ind_Step(RED_pointer, LEDS_PWM_CH_RED, DIR_RED);
-    LEDS_PWM_Ind_Step(BLUE_pointer, LEDS_PWM_CH_BLUE, DIR_BLUE);
-    LEDS_PWM_Ind_Step(GREEN_pointer, LEDS_PWM_CH_GREEN, DIR_GREEN);
-    LEDS_PWM_Ind_Step(ORANGE_pointer, LEDS_PWM_CH_ORANGE, DIR_ORANGE);
-}
-
-void LEDS_PWM_Ind_Step (uint16_t LED_pointer, LEDS_PWM_TIM_CH_e_t TIM_CH, FLAGS_e_t DIR_LED)
-{
-    switch (__FLAG_CHECK(DIR_LED))
+    for (leds_color_e_t color = RED; color < LEDS_ALL; color++)
     {
-        case FORWARD: 
+        switch (leds_config[color].led_dir)
         {
-            if (LED_pointer == LEDS_WAVE_LENGTH)
+            case FORWARD: 
             {
-                __FLAG_RESET(DIR_LED);
+                if (leds_config[color].led_ptr == (LEDS_WAVE_LENGTH - 1))
+                {
+                    leds_config[color].led_dir = REVERSE;
+                }
+                else
+                {
+                    leds_config[color].led_ptr++;
+                }
+                return;
             }
-            else
+            case REVERSE: 
             {
-               LED_pointer++; 
+                if (leds_config[color].led_ptr == 0)
+                {
+                    leds_config[color].led_ptr = (LEDS_WAVE_LENGTH - 1);
+                }
+                else
+                {
+                    leds_config[color].led_ptr--;
+                }
+                return;
             }
-            return;
+            default:
+            {
+                return;
+            }
         }
-        case REVERSE: 
-        {
-            if (LED_pointer == 0)
-            {
-                __FLAG_SET(DIR_LED);
-            }
-            else
-            {
-               LED_pointer--; 
-            }
-            return;
-        }
-        default:
-        {
-            return;
-        }
+    __HAL_TIM_SET_COMPARE(&LEDS_PWM_TIM, leds_config[color].led_ch, LEDS_WAVE[leds_config[color].led_ptr]);
     }
-    __HAL_TIM_SET_COMPARE(&LEDS_PWM_TIM, TIM_CH, LEDS_wave[LED_pointer]);
 }
 /* USER CODE END 0 */
 
@@ -220,10 +190,10 @@ int main(void)
   while (1)
   {
 
-      if (__FLAG_CHECK(LEDS))
+      if (__FLAG_CHECK(LEDS_PWM_CALLB))
       {
           Leds_PWM_Step();
-          __FLAG_RESET(LEDS);
+          __FLAG_RESET(LEDS_PWM_CALLB);
       }
     /* USER CODE END WHILE */
 
@@ -278,7 +248,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
     if(htim == &LEDS_PWM_TIM)
     {
-        __FLAG_SET(LEDS);
+        __FLAG_SET(LEDS_PWM_CALLB);
     }
 }
 /* USER CODE END 4 */
